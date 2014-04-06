d3430570d7606a97ae78f3c3ddb78f942b76a9ca
hoyzhang <hoyzhang@1b99f47e-fe51-0410-be24-b111b41e0848>
2014-04-06 15:35:26 +0000

    异步修改之后，未测试

--- SM2014-JAVA/src/ws/hoyland/sm/ValidateTask.java
+++ SM2014-JAVA/src/ws/hoyland/sm/ValidateTask.java
@@ -1,13 +1,8 @@
 package ws.hoyland.sm;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.params.ConnRouteParams;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.CoreConnectionPNames;
-import org.apache.http.util.EntityUtils;
 
 
 public class ValidateTask implements Runnable {
@@ -22,59 +17,34 @@ public class ValidateTask implements Runnable {
 	}
 
 	public void run() {
-		DefaultHttpClient client = null;
 		HttpGet request = null;
 //		private HttpPost post = null;
 		HttpHost proxy = null;
-		HttpResponse response = null;
-		HttpEntity entity = null;
-		String resp = null;
 		
 		try{
 			String[] ms = line.split(":");
 			proxy = new HttpHost(ms[0], Integer.parseInt(ms[1]));
 	
-			client = new DefaultHttpClient();
-			client.getParams().setParameter(
-					CoreConnectionPNames.CONNECTION_TIMEOUT, 4000);
-			client.getParams().setParameter(
-					CoreConnectionPNames.SO_TIMEOUT, 4000);
-
-			client.getParams().setParameter(ConnRouteParams.DEFAULT_PROXY,
-					proxy);
+			RequestConfig config = RequestConfig.custom()
+				 	.setSocketTimeout(4000)
+	                .setConnectTimeout(4000)
+	                .setConnectionRequestTimeout(4000)
+                    .setProxy(proxy)
+                    .build();
 			
 			String ru = "http://pt.3g.qq.com";
 			request = new HttpGet(ru);
 			request.setHeader("User-Agent", UAG);//
 			request.setHeader("Connection", "close");//
 			
-			response = client.execute(request);
-			entity = response.getEntity();
-			resp = EntityUtils.toString(entity);
-
-			if(resp.indexOf("手机腾讯网")!=-1){
-				synchronized(sb){
-					sb.append(line+"\r\n");
-				}
-			}			
+			request.setConfig(config);
+			
+			Engine.getInstance().validate(sb, line, request);
+				
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
-			try {
-				if (entity != null) {
-					EntityUtils.consume(entity);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			if (request != null) {
-				request.releaseConnection();
-				request.abort();
-			}
-
-			if(client!=null){
-				client.getConnectionManager().shutdown();
-			}
+			
 		}
 	}
 }
